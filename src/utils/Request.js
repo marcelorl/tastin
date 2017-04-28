05fa25d34d4a416616091643ec1908e2ba489d9b
marcelorl <marcelo_rl2@hotmail.com>
2017-04-28 03:08:02 -0300

    adding env conditionally

--- src/utils/Request.js
+++ src/utils/Request.js
@@ -1,5 +1,8 @@
-require('dotenv-safe').config();
 const axios = require('axios');
+
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv-safe').config();
+}
 
 const instance = axios.create({
   baseURL: 'https://maps.googleapis.com/maps/api',
